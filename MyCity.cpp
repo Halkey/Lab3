97749bd497149d23550516b079832d90e07fc09a
Yuuki K <is.yuuki3327@gmail.com>
2016-02-23 22:35:09 -0800

    Initial commit

---
+++ MyCity.cpp
@@ -0,0 +1,25 @@
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+//using namespace std;
+
+int main()
+{
+  int temp1 = 22;
+  int temp2 = 24;
+  int temp3 = 19;
+  int temp4 = 21;
+  int temp5 = 21;
+
+  cout << "Pleasant Hill, High tempuretures" << endl;
+  cout << "Wednesday, " << temp1 << " degrees" << endl;
+  cout << "Thursday, " << temp2 << " degrees" << endl;
+  cout << "Friday, " << temp3 << " degrees" << endl;
+  cout << "Saturday, " << temp4 << " degrees" << endl;
+  cout << "Sunday, " << temp5 << " degrees" << endl;
+  cout << "Source: The Weather Channel" << endl;
+}
---
+++ decl.cpp
@@ -0,0 +1,29 @@
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+//using namespace std;
+
+int main()
+{
+  int age;
+  age = 22;
+
+  double birthWeight;
+  birthWeight = 10.168;
+
+  string firstName;
+  firstName = "Yuki";
+
+  char gender;
+  gender = 'M';
+
+  cout << "My age is " << age << "\n";
+  cout << "My Weight at birth is "
+  << birthWeight << endl;
+  cout << "My first name is " << firstName << endl;
+  cout << "My gender is " << gender << endl;
+}
